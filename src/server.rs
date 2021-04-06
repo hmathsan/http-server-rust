3d2644545641a7c0e6dc09eb4ab4ccca79b3b755
Matheus Santos <matheus.san99@gmail.com>
2021-04-06 16:27:27 -0300

    receiving a request

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,9 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -9,5 +15,34 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening to {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 2048];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+
+                                },
+                                Err(err) => println!("Failed to parse a request: {}", err);
+                            }
+                        },
+                        Err(err) => {
+                            println!("Failed to read from connection: {}", err);
+
+                        }
+                    }
+                },
+                Err(err) => {
+                    println!("Fail to stablish connection: {}", err);
+                    continue;
+                }
+            }
+        }
     }
 }
