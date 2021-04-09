20ef849a3f0fd49713b136acbfc49674a0c6f298
Matheus Santos <matheus.san99@gmail.com>
2021-04-08 22:31:04 -0300

    returning html based on the method and path

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,21 @@
+use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
 
 pub struct  WebsiteHandler;
 
 impl  Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1> TEST </HE>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, 
+                    Some("<h1>404 NOT FOUND</h1>\r\n<h3>Path not found</h3>".to_string()))
+    
+            }
+            _ => Response::new(StatusCode::NotFound, 
+                Some("<h1>404 NOT FOUND</h1>\r\n<h3>Request method not found</h3>".to_string()))
+        }
+        //Response::new(StatusCode::Ok, Some("<h1> TEST </HE>".to_string()))
     }
 }
