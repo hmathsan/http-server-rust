019c28d39c110e2a8a0b7b873bd28d2351d58c53
Matheus Santos <matheus.san99@gmail.com>
2021-04-09 20:59:42 -0300

    fixed some typos

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ mod http;
 mod website_handler;
 
 fn main() {
-    let defaul_path = format!("{}\\public", env!("CARGO_MANIFEST_DIR"));
-    let public_path = env::var("PUBLIC_PATH").unwrap_or(defaul_path);
+    let default_path = format!("{}\\public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     println!("public path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run(WebsiteHandler::new(public_path));
