6aa67c7b78bc2467526b6ea179581bbcfc82417f
Matheus Santos <matheus.san99@gmail.com>
2021-04-08 23:31:01 -0300

    added html file read function

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,21 +1,50 @@
 use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
+use std::fs;
 
-pub struct  WebsiteHandler;
+pub struct  WebsiteHandler {
+    public_path: String
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}\\{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None
+        }
+
+        //fs::read_to_string(path).ok()
+    }
+}
 
 impl  Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, 
-                    Some("<h1>404 NOT FOUND</h1>\r\n<h3>Path not found</h3>".to_string()))
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, 
+                        Some("<h1>404 NOT FOUND</h1>\r\n<h3>Path not found</h3>".to_string()))
+                }
     
             }
             _ => Response::new(StatusCode::NotFound, 
                 Some("<h1>404 NOT FOUND</h1>\r\n<h3>Request method not found</h3>".to_string()))
         }
-        //Response::new(StatusCode::Ok, Some("<h1> TEST </HE>".to_string()))
     }
 }
